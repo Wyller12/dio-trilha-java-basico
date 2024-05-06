1b406d748cea82957b72b74e28b817a1ea5b53f6
wyller12 <wyllerguilherme535@gmail.com>
2024-05-06 20:43:37 -0300

    finalização do módulo de operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,140 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Operadores lógicos
+        
+        
+        
+        
+        
+        
+        
+        //Operadores relacionais
+        /*String nomeUm="wyller";
+        String nomeDois= "wyller";
+        System.out.println(nomeUm.equals(nomeDois));
+        int numero1=1;
+        int numero2=2;
+        boolean simNao= numero1==numero2;
+        if (numero1<numero2){
+            System.out.println("Nossa condição é verdadeira");
+        }
+        System.out.println("Número Um é igual a número dois? " + simNao );
+        simNao = numero1!=numero2;
+        System.out.println("Número Um é diferente de número dois? " + simNao );
+        simNao = numero1 > numero2;
+        System.out.println("Número Um é maior que número dois? " + simNao );
+        simNao = numero1 < numero2;
+        System.out.println("Número Um é menor que número dois? " + simNao );
+
+        //Operador ternário
+        //int a, b;
+
+        //a=5;
+        //b=6;
+        //String resultado = a==b? "verdadeiro" : "falso";
+        //System.out.println(resultado);
+        /*String resultado=("");
+        if (a==b){
+            resultado="verdadeiro";
+
+        }
+        else {
+            resultado="falso";
+
+        }
+        System.out.println(resultado);
+        */
+        
+        
+        
+        
+        
+        
+        
+        
+        //boolean variavel=false;
+        //variavel=true;
+        
+        //System.out.println(variavel);
+
+        
+        
+        
+        
+        
+        
+        //int numero = 5;
+        
+        //x repeticao
+        //numero++;
+        //System.out.println(numero);
+        //System.out.println(++numero);
+        //Operador de atribuição é representado pelo "="
+        //e como o prórpio nome diz, ele serve para atribuir valores as variáveis
+        //Ex:
+        
+        /*String meuNome = "Wyller";
+        int idade = 19;
+        double peso = 63.5;
+        char sexo = 'M';
+        boolean doadorOrgao = true;
+        */
+        
+        //O operador de soma quando é usado em string ele tem a função de concatenação
+        //ex:
+        
+        /*String nome = "LINGUAGEM " + "JAVA";
+        System.out.println(nome);*/
+
+        //Operadores aritiméticos são: + - / % *
+        //Ex:
+        /*double soma = 19.5 + 25.7;
+        int subtração = 113 - 75;
+        int multiplicacao = 29 * 7;
+        int divisao = 75 / 3;
+        int modulo = 19 % 3;
+        double resultado = (10 * 7) + (20/4);*/
+
+        //qual o resultado das expressoes abaixo?
+        /*String concatenacao ="?"; 
+        
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+        
+        concatenacao = 1+"1"+1+1;
+        
+        System.out.println(concatenacao);       
+        
+        concatenacao = 1+"1"+1+"1";
+        
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+1+1+1;
+        
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+(1+1+1);
+        
+        System.out.println(concatenacao);*/
+
+        //Operadores unários
+        //(+) Operador unário de valor positivo – números são positivos sem esse operador explicitamente;
+
+        //(-) Operador unário de valor negativo – nega um número ou expressão aritmética;
+
+        //(++) Operador unário de incremento de valor – incrementa o valor em 1 unidade;
+
+        //(--) Operador unário de decremento de valor – decrementa o valor em 1 unidade;
+
+        //(!) Operador unário lógico de negação – nega o valor de uma expressão booleana.
+        /*int numero = 5;
+        numero = - numero;//transforma o numero positivo em negativo
+        System.out.println(numero);
+        numero = + numero;//Transforma o numero negativo em positivo
+        System.out.println(numero);*/
+
+
+    }
+
+}
